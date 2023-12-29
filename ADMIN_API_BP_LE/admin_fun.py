fe4ca47970a746c55c493986976eb2334a9c617e
sai_leela <rksatya8822@gmail.com>
2023-12-29 18:32:26 +0530

    Initial commit

--- ADMIN_API_BP_LE/admin_fun.py
+++ ADMIN_API_BP_LE/admin_fun.py
@@ -1,27 +1,20 @@
-from pymongo import MongoClient
-client = MongoClient("localhost", 27017)
-db = client["slotzz"]
-dac = db["admin"]
+import datetime
+import re
+# Input validation function for date
+def validate_date(date_str):
+    try:
+        datetime.datetime.strptime(date_str, '%Y-%m-%d')
+        return True
+    except ValueError:
+        False
 
-class Admin_Finder:
- #admin related fucntions
- def admin_finder(phno):
-        """this fn finds the userdetails from the data base by taking in phno as parameter"""
-        filter = {"contact":phno}
-        print(filter)
-        admin_details = dac.find_one(filter = filter)
-        return admin_details
- 
- def emailfinder(emailid):
-         """this fn needs to find the email from database usersdata and return true if present else false """
-         result={"email":emailid}
-         finder = dac.find_one(result)
-         return finder
- def get_admin_data(f_name,l_name,emailid,phno):
-        """this fn inserts  new data  into our database,return if true else false"""
-        admindata={"firstname":f_name,"lastname":l_name,"email":emailid,"contact":phno}
-        admins=dac.insert_one(admindata)
-        if admins.acknowledged:
-            return True
-        else:
-            return False
+# Input validation function for time
+def validate_time(time_str):
+    if re.match(r'^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$', time_str):
+        return True
+    else:
+        False
+
+
+    
+
