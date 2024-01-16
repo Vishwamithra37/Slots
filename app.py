0fda0cce4d0e81fae700c9addab1d65832d72c0a
NavyaSree27 <153295828+NavyaSree27@users.noreply.github.com>
2024-01-16 19:54:21 +0530

    Summary

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import  Flask
+from flask_swagger import swagger
 from ADMIN_API_BP_LE.admin import admin_page
 from USERS_API_BP_NS.users import users_bp
+import os
 
 app = Flask(__name__)
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 app.register_blueprint(admin_page)
 app.register_blueprint(users_bp)
@@ -10,6 +14,12 @@ app.register_blueprint(users_bp)
 @app.route('/')
 def hello():
     return 'Hello World!'
-#trail 
+@app.route("/spec")
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "My API"
+    return jsonify(swag)
+
 if __name__ == '__main__':
     app.run(debug=True)
