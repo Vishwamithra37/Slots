8e96fd90aed5a540434441c1d5bc36e7e36a6ad4
sai_leela <rksatya8822@gmail.com>
2024-01-23 12:20:20 +0530

    new update

--- ADMIN_BP_LEELA/dbops.py
+++ ADMIN_BP_LEELA/dbops.py
@@ -1,7 +1,12 @@
 import datetime
 import re
+from . config import collection  as dac
+
+
+
+class validation:
 # Input validation function for date
-def validate_date(date_str):
+ def validate_date(date_str):
     try:
         datetime.datetime.strptime(date_str, '%Y-%m-%d')
         return True
@@ -9,7 +14,7 @@ def validate_date(date_str):
         False
 
 # Input validation function for time
-def validate_time(time_str):
+ def validate_time(time_str):
     if re.match(r'^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$', time_str):
         return True
     else:
