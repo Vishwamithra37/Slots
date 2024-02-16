6c057d080b9dde821bd7b02fa808eaddc119a9e4
NavyaSree27 <153295828+NavyaSree27@users.noreply.github.com>
2024-02-16 19:11:57 +0530

    modified dbops file

--- USER_BP_NAVYA/dbops.py
+++ USER_BP_NAVYA/dbops.py
@@ -1,39 +1,25 @@
 import flask
 from .config import collection
 import global_config
-from flask_mail import Mail
 from bson import ObjectId
 from bson import json_util
 import os
 import datetime
 from flask import request, session, flash
 
-class DAC(object):
-    def __init__(self, client=None):
-        self.client = client or global_config.MONGOCLIENT
-    
-    def find_one(self, email):
-        return self.client['Account_holders'].find_one({'Email': email})
-    
-    def update_one(self, email, update):
-        return self.client['Account_holders'].update_one({'Email': email}, update)
-    
-    def insert_one(self, data):
-        return self.client['Account_holders'].insert_one(data)
-    
-    def delete_one(self, email):
-        return self.client['Account_holders'].delete_one({'Email': email})
+def find_user_by_email(collection, email):
+    """This function finds users email from Account_holders collection"""
+    user_details = collection.find_one({"Email": email})
+    return user_details
 
-class Mail:
-    def __init__(self, app):
-        self.app = app
-    
-    def send(self, email, subject, message):
-        self.app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-        self.app.config['MAIL_PORT'] = 587
-        self.app.config['MAIL_USE_TLS'] = True
-        self.app.config['MAIL_USER'] = 'navyasri.uv@gmail.com'
-        self.app.config['MAIL_PASSWORD'] = 'navya@1234'
-        self.app.config['MAIL_OPTS'] = {'Client': 'gmail'}
-       # mail = Mail(app)
-        #mail.send(email, subject, message)
+def update_user_fields(collection, email, updated_fields):
+    """This function updates user details(new_contact,new_password) in Account_holders collelction """
+    update_query = {'$set': updated_fields}
+    result = collection.update_one({'Email': email}, update_query)
+    return result.modified_count if result else 0 
+
+
+
+
+
+
